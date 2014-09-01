1e08c88f5644b55726c7f6cce57a9eb9938ddd6c
Daniele Guido <gui.daniele@gmail.com>
2014-09-01 14:37:08 +0200

    google public api key for browser added

--- settings.js
+++ settings.js
@@ -10,4 +10,4 @@ var settings = {
 settings.apiKey = 'AIzaSyCYfU7l7KYDkw9bDOQ6xGR85T0xzJg7MvQ';
 settings.defaultFolder = '0ByZTyEnzm9qqLWVPaE9kaDc4YW8';
 settings.title = 'DRIVE-IN'
-settings.partials = '/src/partials' // basefolder URL for angular partials tamplates
+settings.partials = '/drive-in/src/partials' // basefolder URL for angular partials tamplates
