baa1622dd09232943cd8dcbe65fb45897297a3df
Daniele Guido <gui.daniele@gmail.com>
2014-09-01 14:16:39 +0200

    Create settings.js
    Normally settings.js will be gitignored. This is just for the gh-pages branch

---
+++ settings.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var settings = {
+  baseUrl: 'https://drive.google.com/folderview?id='// the google drive sharing base url
+};
+
+/*
+  Modify according to your own data ...
+*/
+settings.apiKey = 'your (public!) api key';
+settings.defaultFolder = '0ByZTyEnzm9qqLWVPaE9kaDc4YW8';
+settings.title = 'DRIVE-IN'
