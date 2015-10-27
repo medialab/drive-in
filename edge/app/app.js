3e4b1395f4f1797a0eff79ca8695cb6e306e7833
Davy Peter Braun <hello@davybraun.com>
2015-10-27 18:45:11 +0100

    Authenticate, then brodcast event to let the app know.

--- edge/app/app.js
+++ edge/app/app.js
@@ -4,8 +4,49 @@ angular.module('driveIn', [
   'ngRoute',
   'driveIn.init',
   'driveIn.drivein',
-]).
+])
 
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/drive-in'});
+/**
+ * Default route.
+ */
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({
+    redirectTo: '/drive-in'
+  });
+}])
+
+.controller('MainCtrl', ['$scope', '$log', function($scope, $log) {
+  $scope.$on('authenticated', function (e) {
+    $log.info(e)
+  });
+}])
+
+/**
+ * Service to authenticate app for usage of Google Drive API through OAuth2
+ */
+.factory('authenticator', ['$log', function ($log) {
+  var CLIENT_ID = '72848749493-pva84reb1v48u6ddc6l7cukmsso7qib2.apps.googleusercontent.com',
+      SCOPES = [
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'https://www.googleapis.com/auth/drive'
+      ];
+
+  return function (callback) {
+    $log.info('[DRIVE-IN] Attempting OAuth2 authentication.');
+    gapi.auth.authorize({
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, callback);
+  };
+}])
+
+/**
+ * Service to load Drive API upon authentication.
+ */
+.factory('apiLoader', ['$log', function ($log) {
+  $log.info('[DRIVE-IN] Loading Google Drive API.');
+  return function (callback) {
+    gapi.client.load('drive', 'v2', callback);
+  };
 }]);
--- edge/app/views/init/init.js
+++ edge/app/views/init/init.js
@@ -9,6 +9,24 @@ angular.module('driveIn.init', ['ngRoute'])
   });
 }])
 
-.controller('InitCtrl', [function () {
+.controller('InitCtrl', ['$scope', '$log', 'authenticator', 'apiLoader', function ($scope, $log, authenticator, apiLoader) {
+  $scope.authenticate = function () {
+    authenticator(function () {
+      apiLoader(function () {
+        $log.log('[DRIVE-IN] Authenticated. Broadcasting related event.');
+        $scope.$emit('authenticated');
+      });
+    });
+  };
 
-}]);
+  function handleAuthResult(res) {
+    console.log(res);
+  }
+}])
+
+.directive('initializer', function () {
+  return {
+    restrict: 'E',
+    controller: 'InitCtrl',
+  };
+})
