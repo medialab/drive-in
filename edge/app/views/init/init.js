c69937e68f091b227a7c7e8554f572da61c7a067
Davy Peter Braun <hello@davybraun.com>
2015-10-27 20:04:37 +0100

    Start modularizing auth implementation.

--- edge/app/views/init/init.js
+++ edge/app/views/init/init.js
@@ -9,19 +9,40 @@ angular.module('driveIn.init', ['ngRoute'])
   });
 }])
 
+/**
+ * InitCrl is a controller related to the `initializer` directive.
+ */
 .controller('InitCtrl', ['$scope', '$log', 'authenticator', 'apiLoader', function ($scope, $log, authenticator, apiLoader) {
+
+  // Attempt to authenticate on initialization.
+  attemptAuth();
+
+  // If the init (auth) view is visible, and user has not
+  // authorized the app already, the `autorize`
+  // button should trigger a new OAuth2 authentication attempt.
   $scope.authenticate = function () {
-    authenticator(function () {
-      apiLoader(function () {
-        $log.log('[DRIVE-IN] Authenticated. Broadcasting related event.');
-        $scope.$emit('authenticated');
-      });
-    });
+    if (!$scope.isAuthenticated) {
+      return attemptAuth();
+    }
   };
 
-  function handleAuthResult(res) {
-    console.log(res);
+  // Authentication attempt uses the `authenticator` service.
+  // Upon successful authentication, set the related boolean flag to true,
+  // and load Google Drive API.
+  function attemptAuth() {
+    authenticator(function () {
+      $scope.isAuthenticated = true;
+      loadApi();
+    });
   }
+
+  function loadApi() {
+    apiLoader(function () {
+      $log.log('[DRIVE-IN] Authenticated. Broadcasting related event.');
+      $scope.$emit('authenticated');
+    });
+  }
+
 }])
 
 .directive('initializer', function () {
